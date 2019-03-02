9db2b89d110f9f380968e61d6602a97a7079ece1
Trevor Lewis <tenzingtitan@gmail.com>
2019-03-02 13:13:26 -0700

    Sprint Finished!

--- Self-Study/Algorithms_Answers.md
+++ Self-Study/Algorithms_Answers.md
@@ -1,1 +1,18 @@
 Add your answers to the Algorithms exercises here.
+
+Excercise I:
+
+A)
+        O(log n) :
+            The *While* loop will multiply *N* in each loop
+
+B)
+        O(n**3) :
+            The *N* input is required for 3 of the 4 loops.
+
+C)
+        O(n) :
+            *if* statement is constant, but the second return changes the value of bunnies
+
+Excercise II:
+
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -101,7 +101,38 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        #***********Starts turned off**************
+        while self.light_is_on() is False: 
+            self.set_light_on() 
+            
+            while self.can_move_right(): 
+                self.swap_item()
+                self.move_right()
+
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+
+                    self.set_light_off()
+                
+                if self.compare_item() == -1: 
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                
+                if self.compare_item() == 0:
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+            
+
+            
+
+            if self.light_is_on() is False:
+                while self.can_move_left():
+                    self.move_left()
 
 
 if __name__ == "__main__":
